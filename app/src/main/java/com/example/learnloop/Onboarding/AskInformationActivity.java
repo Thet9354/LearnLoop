a5ccbadf0703d4c559cdd4b9425583b0d26cdbf8
PhoonThetPine <thetpine254@gmail.com>
2023-01-31 11:00:52 +0800

    v6- Integrated firebase realtime database (Registration)

--- app/src/main/java/com/example/learnloop/Onboarding/AskInformationActivity.java
+++ app/src/main/java/com/example/learnloop/Onboarding/AskInformationActivity.java
@@ -1,7 +1,10 @@
 package com.example.learnloop.Onboarding;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +18,11 @@ import android.widget.Toast;
 import com.example.learnloop.MainActivity;
 import com.example.learnloop.R;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AskInformationActivity extends AppCompatActivity {
 
@@ -29,6 +37,9 @@ public class AskInformationActivity extends AppCompatActivity {
     private String mName, mEmail, mPassword;
     private String mAllowance, allowanceType, budgetGoal;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://learnloop-1673224439925-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    DatabaseReference databaseReference  = database.getReference().child("users");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,8 +53,6 @@ public class AskInformationActivity extends AppCompatActivity {
         getTransferredData();
 
         pageDirectories();
-
-
     }
 
     private void getTransferredData() {
@@ -87,10 +96,6 @@ public class AskInformationActivity extends AppCompatActivity {
                 validateBudgetGoal();
                 validateInput();
 
-
-//                saveDetails();
-//                Intent intent = new Intent(AskInformationActivity.this, MainActivity.class);
-//                startActivity(intent);
             }
 
 
@@ -100,7 +105,9 @@ public class AskInformationActivity extends AppCompatActivity {
     private void validateInput() {
 
         if (!validateAllowance() | !validateAllowanceType() | !validateBudgetGoal())
-            return;
+        {
+            System.out.println("Rip");
+        }
         else
         {
             addData();
@@ -108,6 +115,60 @@ public class AskInformationActivity extends AppCompatActivity {
     }
 
     private void addData() {
+
+        //Adding data into google realtime database
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Checking if phone number is not registered before
+
+                if (snapshot.hasChild(mName))
+                {
+                    //--->Asking user if he/she wants ti log in to existing account
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AskInformationActivity.this);
+                    builder.setTitle("Mechanica");
+                    builder.setMessage("Hey there, it seems like there's an existing account with the same name.");
+                    builder.setNegativeButton("Register a new account", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                        }
+                    });
+                    builder.setPositiveButton("Log in to existing account", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        }
+                    });
+                    builder.create().show();
+                }
+                else
+                {
+                    System.out.println("Aight");
+                    //--->Adding user's personal information to firebase
+                    databaseReference.child(mName).child("User's Information").child("Full Name").setValue(mName);
+                    databaseReference.child(mName).child("User's Information").child("Email").setValue(mEmail);
+                    databaseReference.child(mName).child("User's Information").child("Password").setValue(mPassword);
+
+                    databaseReference.child(mName).child("User's Information").child("Allowance").setValue(mAllowance);
+                    databaseReference.child(mName).child("User's Information").child("Allowance Type").setValue(allowanceType);
+                    databaseReference.child(mName).child("User's Information").child("Budget Goal").setValue(budgetGoal);
+
+                    Toast.makeText(AskInformationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("Name", mName);
+                    intent.putExtra("Email", mEmail);
+                    intent.putExtra("Password", mPassword);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 
